5a36204a830a44912ed5c6e0d39a53788192541f
Lyra Naeseth <lyra@lyra.sh>
2026-04-24 17:34:08 -0700

    Integrate with new `features` API; remove `Config` (#6)

--- smoke/build.rs
+++ smoke/build.rs
@@ -1,15 +1,13 @@
 use std::path::PathBuf;
 
-use sqlite::{Config, Location, build};
-
 fn main() {
-    let config = Config::default();
+    let config = sqlite::config(None);
 
     let dest = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .parent()
         .expect("workspace dir")
         .join("build");
-    let location = Location::new(dest);
+    let location = sqlite::Location::new(dest);
 
-    build(location, &config);
+    sqlite::build(location, &config);
 }
